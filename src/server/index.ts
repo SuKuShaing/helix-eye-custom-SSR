ec95cdbce1ef2c3b37f07ae9d578a3e7d7f2b1af
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-09-11 17:54:29 -0300

    Hola mundo del servidor

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log('Hola washito carnuo');
+import express, { Express, Request, Response } from "express";
+import { config } from "./config";
+
+const app: Express = express();
+
+app.get('/{*splat}', (req: Request, res: Response) => {
+	res.send(`<h1>Hola Mundo desde la ruta: ${req.url}</h1>`);
+});
+
+app.listen(config.PORT, () => {
+	console.log(`Listening in http://localhost:${config.PORT}`);
+});
